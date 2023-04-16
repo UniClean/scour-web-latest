c8c4624258abf04151e43ae4a5e86c6dd2fb11f4
MilkyWay116 <mils.ergalieva125@gmail.com>
2023-04-16 17:35:53 +0600

    file

--- src/views/Customers/Create.vue
+++ src/views/Customers/Create.vue
@@ -58,16 +58,34 @@
         <p>Переместите файл для загрузки в это поле</p>
 </FileUpload> -->
 
-<FileUpload name="demo[]" url="./upload.php" @upload="onAdvancedUpload($event)" :multiple="true" accept="image/*" :maxFileSize="1000000">
-  <p>Переместите файл для загрузки в это поле</p>
-  <template #chooseButton="{ chooseEnabled }">
-    <button class="p-button p-component" :class="{ 'p-disabled': !chooseEnabled }" style="background-color: blue;">Выберите</button>
-  </template>
-  <template #uploadButton="{ uploadEnabled }">
-    <button class="p-button p-component" :class="{ 'p-disabled': !uploadEnabled }" style="background-color: blue;">Загрузить</button>
-  </template>
-</FileUpload>
 
+<template>
+    <div class="card flex justify-content-center">
+        <Toast />
+        <FileUpload mode="basic" name="demo[]" url="./upload.php" accept="image/*" :maxFileSize="1000000" @upload="onUpload" />
+    </div>
+</template>
+
+
+<template>
+    <div class="card">
+        <Toast />
+        <FileUpload name="demo[]" url="./upload.php" @upload="onAdvancedUpload($event)" :multiple="true" accept="image/*" :maxFileSize="1000000">
+            <template #empty>
+                <p>Drag and drop files to here to upload.</p>
+            </template>
+        </FileUpload>
+    </div>
+</template>
+
+<!-- <script setup>
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
+
+const onAdvancedUpload = () => {
+    toast.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000 });
+};
+</script> -->
            
 
             <Button :label="buttonLabel" class="bg-[#060E28] b-[#060E28] mt-5 mb-5 w-40" @click="validateAndPrepare" />
