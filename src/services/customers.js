2a3d83b78c863dca08773988dc968f4a09b121f2
yollosun <yollollay@gmail.com>
2023-06-03 17:12:31 +0600

    [fix] customer documents

--- src/services/customers.js
+++ src/services/customers.js
@@ -2,39 +2,39 @@ import { api } from "@/plugins/api";
 
 export const getCustomerList = () =>
     api.get('/api/customers/')
-    .then(res => res.data);
+        .then(res => res.data);
 
-    export const getCustomer = (id) => 
+export const getCustomer = (id) =>
     api.get(`/api/customers/${id}/`)
-    .then(res => res.data);
-    
-    export const createCustomer = (customer) => 
+        .then(res => res.data);
+
+export const createCustomer = (customer) =>
     api.post('/api/customers/', customer)
         .then(res => res.data);
-    
-    export const updateCustomer = (id, customer) => 
+
+export const updateCustomer = (id, customer) =>
     api.put(`/api/customers/${id}/update/`, customer)
         .then(res => res.data);
-    
-    export const deleteCustomer = (id) => 
+
+export const deleteCustomer = (id) =>
     api.delete(`/api/customers/${id}/delete/`)
         .then(res => res.data);
 
-    export const uploadFile = (fileFormData) => 
+export const uploadFile = (fileFormData) =>
     api.post(`/api/customers/upload_contract_file/`, fileFormData,
         {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
         })
+        .then(res => res);
+
+export const getAllFiles = (customer_id) =>
+    api.get(`/api/customers/contracts/by-customer-id/${customer_id}/`)
         .then(res => res.data);
 
-    export const getAllFiles = (customer_id) =>
-    api.get(`/api/customers/contracts/by-customer-id/${customer_id}/`)
-    .then(res => res.data);
 
 
-
-    export const displayFile = (id) =>
+export const displayFile = (id) =>
     api.get(`/api/customers/contract/${id}/file/`, { responseType: 'blob' })
         .then(res => res.data);
--- src/views/Orders/components/OrderCard.vue
+++ src/views/Orders/components/OrderCard.vue
@@ -1,8 +1,7 @@
 <template>
-    
     <div class="card flex-1 max-w-sm mx-2 my-2 bg-white rounded-lg shadow-md p-5">
         <div class="card-body">
-            <div class="flex justify-between mb-3" >
+            <div class="flex justify-between mb-3">
                 <div :class="circleClass"></div>
                 <div :class="circleClass"></div>
             </div>
@@ -41,68 +40,68 @@
 
             <div class="card-body__item mt-4">
                 <div class="card-body__item__title">
-                    <p><span class="font-bold">Ответственный супервайзер:</span> {{ order.object.assigned_supervisor.last_name}} 
-                        {{ order.object.assigned_supervisor.first_name}} </p>
+                    <p><span class="font-bold">Ответственный супервайзер:</span> {{
+                        order.object.assigned_supervisor.last_name }}
+                        {{ order.object.assigned_supervisor.first_name }} </p>
                 </div>
                 <div class="card-body__item__content">
                 </div>
             </div>
 
-     <div v-if="order.status == 'PLANNED'|| order.status === 'IN_PROGRESS'"  class="flex justify-end">     
-                <Button class="text-[green] border-[green] mr-1" icon="pi pi-pencil"
-                    @click="() => editOrder(order.id)" />
-                <Button class="text-[red] border-[red] mr-1 " icon="pi pi-trash"
-                    @click="() => deleteOrder(order.id)" />
-        </div>
-
-    <div v-if="order.status == 'COMPLETED'|| order.status === 'CONFIRMED'" class="report mt-5">
-
-        <div class="flex justify-end">
-
-        <Button label="Отчет" class="mt-2 mr-3 border-1 border-black text-black"
-    @click=" showDialog(order.id)" />
-
-    <div v-if="order.status == 'COMPLETED'" >  <Button label="Подтвердить" class="mt-2 border-1 border-black text-black "
-    @click="confirmOrder(order.id)" /></div>
-</div>
-
-<div>
-            <Dialog  :header="'Отчет по уборке объекта: '+  order.object.name " v-model:visible="displayDialog" 
-            style="width: 400px !important; background-color: white;">
-
-            <div class="content">
-
-                <DataTable :value="assignedEmployees">
-                <Column field="employee.first_name" header="Клинер" />
-                <Column field="worked_hours_amount" header="Отработано часов" class="column-custom" />
-                <div v-if="loading" class="flex justify-center items-center">
-            <ProgressSpinner /></div>
-                </DataTable>
-
-                <div class="comment mt-5">
-                    <div class="article-header">
-                        <div class="frame">
-                        <p class="header-text">Комментарий:</p>
-                
-                        <p>{{order.supervisor_comments}}</p>
-                        </div>
-                    </div>
-
-                </div>
-                
+            <div v-if="order.status == 'PLANNED' || order.status === 'IN_PROGRESS'" class="flex justify-end">
+                <Button class="text-[green] border-[green] mr-1" icon="pi pi-pencil" @click="() => editOrder(order.id)" />
+                <Button class="text-[red] border-[red] mr-1 " icon="pi pi-trash" @click="() => deleteOrder(order.id)" />
             </div>
 
-        </Dialog>
+            <div v-if="order.status == 'COMPLETED' || order.status === 'CONFIRMED'" class="report mt-5">
+
+                <div class="flex justify-end">
+
+                    <Button label="Отчет" class="mt-2 mr-3 border-1 border-black text-black"
+                        @click=" showDialog(order.id)" />
+
+                    <div v-if="order.status == 'COMPLETED'"> <Button label="Подтвердить"
+                            class="mt-2 border-1 border-black text-black " @click="confirmOrder(order.id)" /></div>
+                </div>
+
+                <div>
+                    <Dialog :header="'Отчет по уборке объекта: ' + order.object.name" v-model:visible="displayDialog"
+                        style="width: 400px !important; background-color: white;">
+
+                        <div class="content">
+
+                            <DataTable :value="assignedEmployees">
+                                <Column field="employee.first_name" header="Клинер" />
+                                <Column field="worked_hours_amount" header="Отработано часов" class="column-custom" />
+                                <div v-if="loading" class="flex justify-center items-center">
+                                    <ProgressSpinner />
+                                </div>
+                            </DataTable>
+
+                            <div class="comment mt-5">
+                                <div class="article-header">
+                                    <div class="frame">
+                                        <p class="header-text">Комментарий:</p>
+
+                                        <p>{{ order.supervisor_comments }}</p>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </Dialog>
+                </div>
+
+
+            </div>
+
+
+
+
+        </div>
     </div>
-
-
-    </div>
-    
-        
-
-            
-</div>
-</div>
 </template>
 
 
@@ -142,13 +141,13 @@ export default {
             });
         },
 
-    showDialog(orderID) {
-    this.getAssignedEmployees(orderID)
-      this.displayDialog = true;
-      
-    },
-    
-}
+        showDialog(orderID) {
+            this.getAssignedEmployees(orderID)
+            this.displayDialog = true;
+
+        },
+
+    }
     ,
     props: ['order', 'deleteOrder', 'editOrder', 'confirmOrder'],
     computed: {
@@ -165,7 +164,7 @@ export default {
             } else if (status === 'CONFIRMED') {
                 return 'w-3 h-3 bg-green-500 rounded-full border-2 border-black';
             } else {
-                return ''; 
+                return '';
             }
         }
     },
@@ -176,29 +175,27 @@ export default {
 </script>
 
 <style>
-
-
 .card-body {
-    text-align: left; 
+    text-align: left;
 }
 
 .article-header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 10vh;
-  border: 1px solid #060E28;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 10vh;
+    border: 1px solid #060E28;
 }
 
-.column-custom{
+.column-custom {
     display: flex;
-  justify-content: center;
-  align-items: center;
+    justify-content: center;
+    align-items: center;
 }
 
 
 .header-text {
-  font-weight: bold;
-  text-align: center;
+    font-weight: bold;
+    text-align: center;
 }
 </style>
